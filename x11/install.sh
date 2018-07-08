51ff9ccc4e4d426e49cfe534e028a489fefbbad7
Irvin Denzel Torcuato <irvin@wiwi.ninja>
2018-07-08 11:25:51 +0800

    added x11 config

--- x11/install.sh
+++ x11/install.sh
@@ -1,9 +1,19 @@
 #!/bin/sh
 
 X11="`dirname $0`/xresources"
+EXT="`dirname $0`/ext"
+
+init () {
+    # Clean
+    rm -rf ~/.urxvt/ext
+    mkdir -p $HOME/.urxvt/ext
+}
+
+init
 
 echo "Copying xresources to ~/.Xresources"
 cp $X11 $HOME/.Xresources
+cp -r $EXT $HOME/.urxvt
 
 echo "Install complete"
 
