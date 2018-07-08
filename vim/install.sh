10cad60c86a88f5f097512571b65dac4eb819d89
Irvin Denzel Torcuato <irvin@wiwi.ninja>
2018-07-08 11:23:49 +0800

    Installation for windows fixed

--- vim/install.sh
+++ vim/install.sh
@@ -4,20 +4,30 @@ VIMRC="`dirname $0`/vimrc"
 VUNDLE_URL=https://github.com/VundleVim/Vundle.vim
 VUNDLE_DIR=$HOME/.vim/bundle/Vundle.vim
 FILES_DIR=`dirname $0`
+VIMHOME=$HOME/.vim
+VIMHOME_WINDOWS=$HOME/vimfiles
 
-# create vim dirs
-echo "Creating default directories"
-mkdir -p $HOME/.vimswap
-mkdir -p $HOME/.vim/sessions
-mkdir -p $HOME/.vim/bundle
-mkdir -p $HOME/.vim/ftplugin
-mkdir -p $HOME/.vim/syntax
+init () {
+    if [ -d $VIMHOME_WINDOWS ]; then
+        rm -Rf $VIMHOME_WINDOWS
+    fi
+
+    # create vim dirs
+    echo "Creating default directories"
+    mkdir -p $HOME/.vimswap
+    mkdir -p $VIMHOME/sessions
+    mkdir -p $VIMHOME/bundle
+    mkdir -p $VIMHOME/ftplugin
+    mkdir -p $VIMHOME/syntax
+}
+
+init
 
 echo "Copying files dir"
-cp -r $FILES_DIR/mclinica $HOME/.vim/.
-cp -r $FILES_DIR/snippets $HOME/.vim/.
-cp -r $FILES_DIR/ftplugin $HOME/.vim/.
-cp -r $FILES_DIR/syntax $HOME/.vim/.
+cp -r $FILES_DIR/mclinica $VIMHOME/.
+cp -r $FILES_DIR/snippets $VIMHOME/.
+cp -r $FILES_DIR/ftplugin $VIMHOME/.
+cp -r $FILES_DIR/syntax $VIMHOME/.
 
 # Copy vimrc
 echo "Copying .vimrc"
@@ -26,8 +36,10 @@ cp $VIMRC $HOME/.vimrc
 # Install vundle
 if [ ! -e $VUNDLE_DIR ];
 then
-	git clone $VUNDLE_URL $VUNDLE_DIR
+    git clone $VUNDLE_URL $VUNDLE_DIR
 fi
+
+ln -Ts $VIMHOME $VIMHOME_WINDOWS
 
 # Install Bundles
 vim +PluginInstall +qa
