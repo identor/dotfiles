03ebb9643fdeba277ac13ba0f29c638f30329bc5
Irvin Torcuato <irvin@wiwi.ninja>
2019-06-10 00:05:07 +0800

    Updates 201906100005

--- zsh/install.sh
+++ zsh/install.sh
@@ -5,6 +5,12 @@ if ! [ -d $HOME/.oh-my-zsh ]; then
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 fi
 
+K_ALIAS_HOME=$HOME/.kubectl_aliases
+# Install kubectl-aliases
+if ! [ -f $K_ALIAS_HOME ]; then
+  curl https://raw.githubusercontent.com/ahmetb/kubectl-aliases/master/.kubectl_aliases -o $K_ALIAS_HOME
+fi
+
 ZSHRC="`dirname $0`/zshrc"
 
 echo "Copying zshrc"
